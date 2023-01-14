3a56937bf12c99b28d3c27cc3ab466547d120b43
Panchajanya1999 <kernel@panchajanya.dev>
2023-01-14 21:56:09 +0530

    exec: jump if execution fails
    they missed it?
    
    Signed-off-by: Panchajanya1999 <kernel@panchajanya.dev>
    (cherry picked from commit 75d5a584d24af8a58c060d67d31bbf3dc3ce5290)
    (cherry picked from commit a46b246882a3800300d9f10fd243a94d89f7a6f9)

--- fs/exec.c
+++ fs/exec.c
@@ -1950,6 +1950,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 	}
 
 	retval = bprm_execve(bprm, fd, filename, flags);
+	if (retval < 0)
+		goto out_free;
+
 out_free:
 	free_bprm(bprm);
 
